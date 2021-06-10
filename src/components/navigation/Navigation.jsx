bfc52d9c90a6e40b19ed7453a08651d0b20ed0f4
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-06-10 14:37:34 +0200

    push main tartine pt3

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,15 +4,28 @@ import './navigation.css'
 import Search_bar from '../search_bar/Search_bar'
 
 class Navigation extends React.Component{
+
+    showAccueil = () => {
+        console.log('Accueil');
+    }
+
+    showGalerie = () => {
+        console.log('Galerie');
+    }
+
+    showContact = () => {
+        console.log('Contact');
+    }
+
     render() {
         return(
             <div className='navigation_div'>
                 <nav className='navigation_div_nav'>
                     <div className="navigation_div_nav_left">
                         <ul>
-                            <li><a>Link 1</a></li>
-                            <li><a>Link 2</a></li>
-                            <li><a>Link 3</a></li>
+                            <li onClick={this.showAccueil} ><a>Link 1</a></li>
+                            <li onMouseOver={this.showGalerie} ><a>Link 2</a></li>
+                            <li onDoubleClick={this.showContact} ><a>Link 3</a></li>
                         </ul>
                     </div>
                     <div className="navigation_div_nav_right">
