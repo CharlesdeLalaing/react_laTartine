7778e9ff32e84c0a676d5c68298543d49ebc03a0
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-06-10 14:29:02 +0200

    push main tartine pt 2

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -5,11 +5,28 @@ import Article from '../article/Article'
 
 
 class Body extends React.Component {
+    
+    articlesEnCours = 2;
+
     render() {
         return (
-            <div className='body_div'>
-                    <Article/>
-                    <Article/>
+            //Condition à la volée avec l’opérateur logique &&
+            <div className='body_div'> 
+
+                { this.articlesEnCours >= 2 &&
+                    <div className="articles">
+                        <Article/>
+                        <Article/>                        
+                    </div>
+                }                
+
+
+                { this.articlesEnCours < 2 &&
+                    <div className="articles text-center text-white">
+                        <h1>fuck you m8</h1>                    
+                    </div>
+                }
+
             </div>
         );
     }
